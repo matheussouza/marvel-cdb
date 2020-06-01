ff896906ccfce8b23aa8c9506a1fab6e11ffd5f3
Matheus Souza <matheusdemssouza@gmail.com>
2020-06-01 08:15:18 -0300

    Setting keys to test

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@ import md5 from 'md5';
 
 export const authorizationQuery = () => {
   const timestamp = Date.now();
-  const publicKey = process.env.REACT_APP_API_PUBLIC_KEY;
-  const privateKey = process.env.REACT_APP_API_PRIVATE_KEY;
+  const publicKey = process.env.REACT_APP_API_PUBLIC_KEY || '917edf86283eda89772bac2a2540e2b9';
+  const privateKey = process.env.REACT_APP_API_PRIVATE_KEY || '0d94e73cc1508813c1b2c1eedef382646d84d5ce';
   return `apikey=${publicKey}&ts=${timestamp}&hash=${md5(timestamp + privateKey + publicKey)}`;
 }
 
